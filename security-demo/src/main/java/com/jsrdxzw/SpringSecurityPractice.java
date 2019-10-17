fa9c031a6890088d2f9319763f77007130cd886c
xuzhiwei <jsrdxzw@163.com>
2019-10-17 23:20:21 +0900

    init project

---
+++ security-demo/src/main/java/com/jsrdxzw/SpringSecurityPractice.java
@@ -0,0 +1,16 @@
+package com.jsrdxzw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: xuzhiwei
+ * @Date: 2019/10/17
+ * @Description:
+ */
+@SpringBootApplication
+public class SpringSecurityPractice {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPractice.class, args);
+    }
+}
